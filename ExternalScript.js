72f69ca221e32adbd570714d95b9629d1011bbec
SRIDHARALAPATI <sridharalapati22@gmail.com>
2023-12-20 00:47:44 -0600

    Javascript learnings

---
+++ ExternalScript.js
@@ -0,0 +1,11 @@
+//Hositing
+getName();
+console.log(num);
+console.log(getName);
+
+var num=2;
+function getName(){
+  console.log("Sridhar Reddy Alapati")
+}
+// console.log(num);
+// console.log(getName());
