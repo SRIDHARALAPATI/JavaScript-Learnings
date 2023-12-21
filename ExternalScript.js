a5a251284ebd2f0e4fdc2d8bd0ba30ccc52f9e4b
SRIDHARALAPATI <sridharalapati22@gmail.com>
2023-12-21 00:07:32 -0600

    Javascript learnings

--- ExternalScript.js
+++ ExternalScript.js
@@ -1,11 +1,24 @@
 //Hositing
-getName();
+/*getName();
 console.log(num);
 console.log(getName);
 
 var num=2;
 function getName(){
   console.log("Sridhar Reddy Alapati")
-}
+}*/
 // console.log(num);
 // console.log(getName());
+
+/********************************************************************************************* */
+// Scope chain example and debugging for understanding
+
+function a(){
+  var s="sridhar";
+  b();
+  function b(){
+    console.log(s);
+  }
+}
+
+a();
