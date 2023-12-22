bcb2d920c865326d2c2383a74cbfc0a50d2609ac
SRIDHARALAPATI <sridharalapati22@gmail.com>
2023-12-21 19:09:54 -0600

    added block-level example

--- ExternalScript.js
+++ ExternalScript.js
@@ -1,12 +1,12 @@
 //Hositing
-/*getName();
+getName();
 console.log(num);
 console.log(getName);
 
 var num=2;
 function getName(){
   console.log("Sridhar Reddy Alapati")
-}*/
+}
 // console.log(num);
 // console.log(getName());
 
@@ -22,3 +22,18 @@ function a(){
 }
 
 a();
+
+/********************************************************************************************* */
+// block scope  example and its behavaiour in browser
+
+{
+  var x=10;
+  let y=100;
+  const z= 1000;
+  console.log(x);
+  console.log(y);
+  console.log(z);
+}
+console.log(x); //we can access because it is gloabl -leval acceess
+console.log(y); // we can't access  variable because is y is declared with let , it is a blockl-level scope
+console.log(z);// we can't access  variable because is z is declared with const , it is a blockl-level scope
